87eb846dc4c29319e01f46bd219580770aaa5e3b
Edmond Tabaku <edmondtabaku20@gmail.com>
2023-01-04 18:06:07 +0100

    - Initial commit

---
+++ dummy.js
@@ -0,0 +1,6 @@
+export const events = [
+    {
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6vUTWn4BFppuv43nUbILcFeDS-_Vx4aOy9pM6_rE1PIhK_pkbGqfq05MoKOdZO6h7VFs&usqp=CAU',
+        name:'Christmas'
+    }
+]
